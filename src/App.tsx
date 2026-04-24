c27513be5ba71d7729f5f539ea391b8cfb58c506
Park-min-hyoung <qkralswgud1324@gmail.com>
2026-04-24 18:10:01 +0900

    App 컴포넌트 import 미리 준비

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import "./App.css";
 // import SelectionControlAnswer from "./days/day02/selection-control.answer";
 // import TreeWalker from "./days/day03/tree-walker";
 // import TreeWalkerAnswer from "./days/day03/tree-walker.answer";
-import CarectControl from "./days/day04/caret-control";
-import CarectControlAnswer from "./days/day04/caret-control.answer";
+// import CarectControl from "./days/day04/caret-control";
+// import CarectControlAnswer from "./days/day04/caret-control.answer";
+import CaretCoordinatesAnswer from "./days/day05/caret-coordinates.answer.tsx";
+import CaretCoordinates from "./days/day05/caret-coordinates.tsx";
+// import HighlightApiAnswer from "./days/day06/highlight-api.answer.tsx";
+// import HighlightApi from "./days/day06/highlight-api.tsx";
+// import FloatingToolbarAnswer from "./days/day07/floating-toolbar.answer.tsx";
+// import FloatingToolbar from "./days/day07/floating-toolbar.tsx";
+// import InsertBlockHeadingAnswer from "./days/day08/insert-block-heading.answer.tsx";
+// import InsertBlockHeading from "./days/day08/insert-block-heading.tsx";
+// import ToggleMarkBoldAnswer from "./days/day09/toggle-mark-bold.answer.tsx";
+// import ToggleMarkBold from "./days/day09/toggle-mark-bold.tsx";
+// import ToggleMarkMultiAnswer from "./days/day10/toggle-mark-multi.answer.tsx";
+// import ToggleMarkMulti from "./days/day10/toggle-mark-multi.tsx";
 
 function App() {
   return (
@@ -15,13 +27,44 @@ function App() {
         gap: "200px",
       }}
     >
+      {/* Day01 */}
       {/* <RangeBasicsDemo /> */}
+
+      {/* Day02 */}
       {/* <SelectionControl /> */}
       {/* <SelectionControlAnswer /> */}
+
+      {/* Day03 */}
       {/* <TreeWalkerAnswer />
       <TreeWalker /> */}
-      <CarectControlAnswer />
-      <CarectControl />
+
+      {/* Day04 */}
+      {/* <CarectControlAnswer />
+      <CarectControl /> */}
+
+      {/* Day05 */}
+      <CaretCoordinatesAnswer />
+      <CaretCoordinates />
+
+      {/* Day06 */}
+      {/* <HighlightApiAnswer />
+      <HighlightApi /> */}
+
+      {/* Day07 */}
+      {/* <FloatingToolbarAnswer />
+      <FloatingToolbar /> */}
+
+      {/* Day08 */}
+      {/* <InsertBlockHeadingAnswer />
+      <InsertBlockHeading /> */}
+
+      {/* Day09 */}
+      {/* <ToggleMarkBoldAnswer />
+      <ToggleMarkBold /> */}
+
+      {/* Day10 */}
+      {/* <ToggleMarkMultiAnswer />
+      <ToggleMarkMulti /> */}
     </div>
   );
 }
